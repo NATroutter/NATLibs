a296f12488fff73ac79361df7508a751ecfa5a0d
NATroutter <natroutter@gmail.com>
2022-04-29 21:46:30 +0300

    Added missing files

---
+++ src/net/natroutter/natlibs/handlers/LangHandler/language/key/LanguageKey.java
@@ -0,0 +1,29 @@
+package net.natroutter.natlibs.handlers.LangHandler.language.key;
+
+import org.jetbrains.annotations.NotNull;
+
+public class LanguageKey {
+
+    private final @NotNull String code;
+
+    private LanguageKey(@NotNull String code) {
+        this.code = code;
+    }
+
+    public @NotNull String getCode() {
+        return code;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof LanguageKey) {
+            LanguageKey languageKey = (LanguageKey) obj;
+            return languageKey.code.equals(this.code);
+        }
+        return false;
+    }
+
+    public static LanguageKey of(String code) {
+        return new LanguageKey(code);
+    }
+}
---
+++ src/net/natroutter/natlibs/handlers/LangHandler/language/key/TranslationKey.java
@@ -0,0 +1,29 @@
+package net.natroutter.natlibs.handlers.LangHandler.language.key;
+
+import org.jetbrains.annotations.NotNull;
+
+public class TranslationKey {
+
+    private final @NotNull String key;
+
+    private TranslationKey(@NotNull String key) {
+        this.key = key;
+    }
+
+    public @NotNull String getKey() {
+        return key;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof TranslationKey) {
+            TranslationKey languageKey = (TranslationKey) obj;
+            return languageKey.key.equals(this.key);
+        }
+        return false;
+    }
+
+    public static TranslationKey of(String key) {
+        return new TranslationKey(key);
+    }
+}
