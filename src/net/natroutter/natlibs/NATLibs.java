ef433ba443d43c9626f4fe1f82b1c82bf0bd5cec
Zoltus <noatuiskula@gmail.com>
2022-09-16 19:26:28 +0300

    - Made jar smaller by adding runtime libraries also minimized jar shading option

--- src/net/natroutter/natlibs/NATLibs.java
+++ src/net/natroutter/natlibs/NATLibs.java
@@ -1,18 +1,31 @@
 package net.natroutter.natlibs;
 
+import net.natroutter.natlibs.events.PlayerJumpEvent.PlayerJumpEventListener;
 import net.natroutter.natlibs.files.Config;
 import net.natroutter.natlibs.handlers.VersionChecker;
 import net.natroutter.natlibs.handlers.configuration.ConfigManager;
+import net.natroutter.natlibs.handlers.gui.GUIListener;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.dependency.Libraries;
+import org.bukkit.plugin.java.annotation.dependency.Library;
+import org.bukkit.plugin.java.annotation.plugin.Description;
+import org.bukkit.plugin.java.annotation.plugin.LogPrefix;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.Website;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
-import net.natroutter.natlibs.events.PlayerJumpEvent.PlayerJumpEventListener;
-import net.natroutter.natlibs.handlers.gui.GUIListener;
-import org.jetbrains.annotations.NotNull;
-
+@Plugin(name = "NATLibs", version = "2.0.12")
+@Description("NATroutter's library for plugin Developement")
+@Author("NATroutter")
+@Website("https://plugins.nat.gg/plugin/natlibs/")
+@LogPrefix("NATLibs")
+@Libraries({
+		@Library("org.jsoup:jsoup:1.15.3")
+})
 public class NATLibs extends JavaPlugin {
 
 	//TODO
